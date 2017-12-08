a289ca81e64a48e242dd50adcedbfad78341876e
Matt Reynolds <matt.reynolds@autologic.com>
2017-12-08 19:04:29 +0000

    2017 day 7

---
+++ tests/test_day_07.py
@@ -0,0 +1,41 @@
+import days
+
+def test_1():
+    tests = [
+        ('''pbga (66)
+            xhth (57)
+            ebii (61)
+            havc (66)
+            ktlj (57)
+            fwft (72) -> ktlj, cntj, xhth
+            qoyq (66)
+            padx (45) -> pbga, havc, qoyq
+            tknk (41) -> ugml, padx, fwft
+            jptl (61)
+            ugml (68) -> gyxo, ebii, jptl
+            gyxo (61)
+            cntj (57)''', 
+        'tknk'),
+    ]
+    for t in tests:
+        assert days.day_07(t[0])['result'] == t[1]
+
+def test_2():
+    tests = [
+        ('''pbga (66)
+            xhth (57)
+            ebii (61)
+            havc (66)
+            ktlj (57)
+            fwft (72) -> ktlj, cntj, xhth
+            qoyq (66)
+            padx (45) -> pbga, havc, qoyq
+            tknk (41) -> ugml, padx, fwft
+            jptl (61)
+            ugml (68) -> gyxo, ebii, jptl
+            gyxo (61)
+            cntj (57)''', 
+        60),
+    ]
+    for t in tests:
+        assert days.day_07_part_2(t[0])['result'] == t[1]
